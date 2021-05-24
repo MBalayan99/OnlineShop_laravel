e18a6f7123b01ea18fd5cb368d2d52b1194f551e
Mher2019 <balayan.mher88@gmail.com>
2021-05-24 12:34:31 +0400

    create products  seeder

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -623,6 +623,7 @@ class ComposerStaticInit54cde1513c7445ce6611d00241910788
         'Cron\\MonthField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/MonthField.php',
         'Database\\Factories\\UserFactory' => __DIR__ . '/../..' . '/database/factories/UserFactory.php',
         'Database\\Seeders\\DatabaseSeeder' => __DIR__ . '/../..' . '/database/seeders/DatabaseSeeder.php',
+        'Database\\Seeders\\ProductstableSeeder' => __DIR__ . '/../..' . '/database/seeders/ProductstableSeeder.php',
         'DeepCopy\\DeepCopy' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/DeepCopy.php',
         'DeepCopy\\Exception\\CloneException' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/Exception/CloneException.php',
         'DeepCopy\\Exception\\PropertyException' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/Exception/PropertyException.php',
